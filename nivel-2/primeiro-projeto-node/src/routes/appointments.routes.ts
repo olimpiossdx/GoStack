66afd891a49a26d08beacdeb1b505fb198cd627a
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-08 09:57:49 -0300

    Configure and fix ESLint

--- nivel-2/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ nivel-2/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', (request, response) => response.json({ message: 'Hello Gostack' }));
 
-  return response.json({ message: 'Hello Gostack' });
-});
 export default appointmentsRouter;
--- nivel-2/primeiro-projeto-node/src/server.ts
+++ nivel-2/primeiro-projeto-node/src/server.ts
@@ -5,4 +5,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+/* eslint no-console: ["error", { allow: ["log"] }] */
 app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
