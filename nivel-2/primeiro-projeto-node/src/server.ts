6b39445021f415495d365cde208c0dfc55f83f7c
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-07 21:52:33 -0300

    Project structure organized

--- nivel-2/primeiro-projeto-node/src/server.ts
+++ nivel-2/primeiro-projeto-node/src/server.ts
@@ -1,18 +1,8 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello goStack' });
-});
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = { name, email };
-
-  return response.json(user);
-});
-
+app.use(routes);
 app.listen(3333, () => console.log('🚀 Server started on port 3333!'));
