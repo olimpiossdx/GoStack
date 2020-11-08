f78a593641cd77f19aff80a49b8306c49db4c1ac
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-08 20:10:35 -0300

    Add create user

--- nivel-2/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ nivel-2/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,7 +1,22 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.post('/', (request, response) => response.json({ message: 'Hello Gostack' }));
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
 
 export default appointmentsRouter;
