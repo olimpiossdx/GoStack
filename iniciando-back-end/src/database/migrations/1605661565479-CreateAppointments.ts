f1a0b39a8d7f095e2b9eb387f43e146f6ead1f69
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-21 15:50:05 -0300

    Updated migrations, type columns, relationship user e e appointments

--- iniciando-back-end/src/database/migrations/1605661565479-CreateAppointments.ts
+++ iniciando-back-end/src/database/migrations/1605661565479-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1605661565479 implements MigrationInterfa
       columns: [
         {
           name: 'id',
-          type: 'varchar',
+          type: 'uuid',
           isPrimary: true,
           generationStrategy: 'uuid',
           default: 'uuid_generate_v4()'
