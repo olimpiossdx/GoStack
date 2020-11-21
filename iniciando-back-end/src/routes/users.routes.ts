aee55b1d9b227a3ba6d601328184bfe78616ffb4
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-21 20:35:37 -0300

    Finished error handling

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -10,30 +10,20 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUserService = new CreateUserService();
+  const { name, email, password } = request.body;
+  const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, email, password });
+  const user = await createUserService.execute({ name, email, password });
 
-    return response.json(user);
-
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  try {
-    const updateUserAvatar = new UpdateUserAvatarService();
+  const updateUserAvatar = new UpdateUserAvatarService();
 
-    const user = await updateUserAvatar.execute({ user_id: request.user.id, avatarFilename: request.file.filename });
+  const user = await updateUserAvatar.execute({ user_id: request.user.id, avatarFilename: request.file.filename });
 
-    response.json(user);
-
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  response.json(user);
 });
 
 export default usersRouter;
