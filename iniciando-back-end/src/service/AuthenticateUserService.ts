56a892721b5c9eb6a6174a58641d1071dc1fc7c0
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-21 17:34:02 -0300

    Add assign user token session

--- iniciando-back-end/src/service/AuthenticateUserService.ts
+++ iniciando-back-end/src/service/AuthenticateUserService.ts
@@ -1,4 +1,5 @@
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import { getRepository } from "typeorm";
 import User from "../models/User";
 
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,9 @@ class AuthenticateUserService {
       throw new Error('Incorret email/password combination.');
     }
 
-    return { user };
+    const token = sign({}, ' 2eccf18a7fa2854163ecb01e25579049', { subject: user.id, expiresIn: '1d' });
+
+    return { user, token };
   }
 };
 
