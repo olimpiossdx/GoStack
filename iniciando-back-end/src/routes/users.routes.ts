37aabce46511d6f58ef2f206e7b409a0a506900b
Olimpio Pimenta <olimpiodx@gmail.com>
2020-11-21 19:41:13 -0300

    Inital upload file avatar

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
-import { Router } from 'express';
+import { request, response, Router } from 'express';
+import multer from 'multer';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../service/CreateUserService';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -15,6 +20,11 @@ usersRouter.post('/', async (request, response) => {
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  console.log('request.file', request.file);
+  response.json({ message: 'alterado' });
 });
 
 export default usersRouter;
