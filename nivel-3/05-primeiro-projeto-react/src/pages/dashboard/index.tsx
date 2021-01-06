120cdfefa37af873bf8196068bd7116058aecf03
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-05 21:11:40 -0300

    Estilização do dasboard

--- nivel-3/05-primeiro-projeto-react/src/pages/dashboard/index.tsx
+++ nivel-3/05-primeiro-projeto-react/src/pages/dashboard/index.tsx
@@ -1,13 +1,57 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return (
+  return (<>
+    <img src={logoImg} alt='Github explore' />
     <Title>
       Explore repositórios no GitHub.
-    </Title>
-  )
+  </Title>
+    <Form>
+      <input placeholder='Digite o nome do repositório' />
+      <button type='submit'>
+        Pesquisar
+      </button>
+    </Form>
+    <Repositories>
+      <a href='teste'>
+        <img src={logoImg} alt='Dev' />
+        <div>
+          <strong>
+            texto 1
+          </strong>
+          <p> texto 2</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href='teste'>
+        <img src={logoImg} alt='Dev' />
+        <div>
+          <strong>
+            texto 1
+          </strong>
+          <p> texto 2</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href='teste'>
+        <img src={logoImg} alt='Dev' />
+        <div>
+          <strong>
+            texto 1
+          </strong>
+          <p> texto 2</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>)
 }
 
 export default Dashboard;
