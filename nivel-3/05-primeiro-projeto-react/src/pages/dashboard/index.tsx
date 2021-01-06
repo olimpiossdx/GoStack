37ca6856b725bdd38136b88c76c2c959700425d8
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-05 22:03:04 -0300

    Conectando com api do github

--- nivel-3/05-primeiro-projeto-react/src/pages/dashboard/index.tsx
+++ nivel-3/05-primeiro-projeto-react/src/pages/dashboard/index.tsx
@@ -4,54 +4,64 @@ import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = React.useState('');
+  const [repostories, setRepostories] = React.useState<Repository[]>([]);
+
+  async function handleAddRepository(event: React.FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepostories([...repostories, repository]);
+    setNewRepo('');
+  }
+
   return (<>
     <img src={logoImg} alt='Github explore' />
     <Title>
       Explore repositórios no GitHub.
   </Title>
-    <Form>
-      <input placeholder='Digite o nome do repositório' />
+    <Form onSubmit={handleAddRepository}>
+      <input
+        value={newRepo}
+        onChange={(e) => setNewRepo(e.target.value)}
+        placeholder='Digite o nome do repositório'
+      />
       <button type='submit'>
         Pesquisar
       </button>
     </Form>
     <Repositories>
-      <a href='teste'>
-        <img src={logoImg} alt='Dev' />
-        <div>
-          <strong>
-            texto 1
-          </strong>
-          <p> texto 2</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-
-      <a href='teste'>
-        <img src={logoImg} alt='Dev' />
-        <div>
-          <strong>
-            texto 1
-          </strong>
-          <p> texto 2</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-
-      <a href='teste'>
-        <img src={logoImg} alt='Dev' />
-        <div>
-          <strong>
-            texto 1
-          </strong>
-          <p> texto 2</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
+      {repostories.map(repository => (
+        <a href={'teste'}>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <div>
+            <strong>
+              {repository.full_name}
+            </strong>
+            <p>{repository.description}</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      ))}
     </Repositories>
   </>)
 }
 
 export default Dashboard;
+
