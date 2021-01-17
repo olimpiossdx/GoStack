a9e9ada3ca45d0dcc2f05dfed4f4b23582c8a21f
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-17 18:34:07 -0300

    Altualização de plugins e libs para padronização de projeto

--- nivel-3/06-gobarber-web/src/App.tsx
+++ nivel-3/06-gobarber-web/src/App.tsx
@@ -1,10 +1,10 @@
+import GlobalStyles from './styles/globa';
 
-const App: React.FC = () => {
-  return (
-    <h1>
-      Hello world
-    </h1>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <h1>Hello world</h1>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
