54d095d5d8a109f30f836ae231fcb5d79368f4b9
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-17 20:38:34 -0300

    Página de cadastro

--- nivel-3/06-gobarber-web/src/App.tsx
+++ nivel-3/06-gobarber-web/src/App.tsx
@@ -1,9 +1,10 @@
 import GlobalStyles from './styles/globa';
 import SignIn from './pages/Signin';
+import SignUP from './pages/SignUP';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUP />
     <GlobalStyles />
   </>
 );
---
+++ nivel-3/06-gobarber-web/src/pages/SignUP/index.tsx
@@ -0,0 +1,40 @@
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.svg';
+
+const SignUP = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUP;
