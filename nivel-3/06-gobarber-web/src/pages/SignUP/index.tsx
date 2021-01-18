ea985f8627952bff0a8e75a19dc841d7285007ef
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-17 21:36:04 -0300

    Adicionando lib para gerenciar validação de formulário

--- nivel-3/06-gobarber-web/src/pages/SignUP/index.tsx
+++ nivel-3/06-gobarber-web/src/pages/SignUP/index.tsx
@@ -1,15 +1,27 @@
+import { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-
 import logoImg from '../../assets/logo.svg';
 
 const SignUP = () => {
-  function handleSubmit(data: any): void {
-    console.log('data', data);
-  }
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido.'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
