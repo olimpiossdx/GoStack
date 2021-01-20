a7e2eb1932e541dc37473bd7873279e0a8d70339
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-20 19:44:37 -0300

    Criando estrutura de Toast

--- nivel-3/06-gobarber-web/src/App.tsx
+++ nivel-3/06-gobarber-web/src/App.tsx
@@ -3,12 +3,14 @@ import SignIn from './pages/Signin';
 import SignUP from './pages/SignUP';
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyles />
   </>
 );
