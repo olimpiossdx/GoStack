464ca0308c62e01c079589edd5d5182c15c7b8b1
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-21 22:42:10 -0300

    Rotas autenticadas

--- nivel-3/06-gobarber-web/src/routes/index.tsx
+++ nivel-3/06-gobarber-web/src/routes/index.tsx
@@ -1,12 +1,17 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/Signin';
 import SignUP from '../pages/SignUP';
+
+import Route from './Route';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUP} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
