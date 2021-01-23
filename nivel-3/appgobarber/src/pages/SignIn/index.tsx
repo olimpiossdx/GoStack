60a22cc5c58ed8ee72cb01fdcc3b75d6bcc67a04
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-23 09:54:36 -0300

    Criando estilizações para Input e Button

--- nivel-3/appgobarber/src/pages/SignIn/index.tsx
+++ nivel-3/appgobarber/src/pages/SignIn/index.tsx
@@ -3,14 +3,20 @@ import { View, Text, Image } from 'react-native';
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name='email' icon='mail' placeholder='E-mail' />
+      <Input name='password' icon='lock' placeholder='Senha' />
+      <Button onPress={() => { console.log('clicou') }} >Entrar</Button>
     </Container>
   )
 }
 
 export default SignIn;
+
