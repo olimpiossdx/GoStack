4b21d84bc5280a43698c9c13e48c8154bebcf191
Olimpio Pimenta <olimpiodx@gmail.com>
2021-01-26 23:10:41 -0300

    Pensando nos testes

--- iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,10 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    //TODO: completar com o teste
+  });
+
+  it('should not be able to create tow appointments on the same time', () => {
+    //TODO: completar com o teste
+  });
+
 });
