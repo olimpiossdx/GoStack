23efe96308aeb236773a256b0931e7f361e717c2
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-07 15:34:00 -0300

    Proximo agendamento

--- nivel-3/06-gobarber-web/src/pages/Dashboard/index.tsx
+++ nivel-3/06-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Calendar,
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  NextAppointment,
+  Profile,
+  Schedule,
+} from './styles';
 
 // eslint-disable-next-line import/no-unresolved
 import logoImg from '../../assets/logo.svg';
@@ -27,6 +36,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horário agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/30667729?s=460&u=e5e7f27ae6315a30400120e97b64a61ea4cc83fc&v=4"
+                alt="OLimpio pimenta"
+              />
+              <strong>Olimpio Pimenta</strong>
+              <span>
+                <FiClock />
+                8:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
