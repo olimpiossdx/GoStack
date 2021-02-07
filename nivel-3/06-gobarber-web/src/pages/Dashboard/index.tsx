e18b60ed035f1d5e6ccb5c0f9200426a53f30865
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-07 19:52:17 -0300

    Agendamentos da API

--- nivel-3/06-gobarber-web/src/pages/Dashboard/index.tsx
+++ nivel-3/06-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable camelcase */
+/* eslint-disable import/no-duplicates */
 import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import { isToday, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
 import { FiClock, FiPower } from 'react-icons/fi';
-import { date } from 'yup';
 import {
-  Appointment,
+  Appointment as IAppointment,
   Calendar,
   Container,
   Content,
@@ -27,13 +30,25 @@ interface IMonthAvailabilityItem {
   available: boolean;
 }
 
+interface IAppointment {
+  id: string;
+  date: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [currentMonth, setCurrentMonth] = useState(new Date());
+
   const [monthAvailability, setMonthAvailability] = useState<
     IMonthAvailabilityItem[]
   >([]);
+
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
     if (modifiers.available) {
@@ -58,6 +73,20 @@ const Dashboard: React.FC = () => {
       });
   }, [currentMonth, user.id]);
 
+  useEffect(() => {
+    api
+      .get('/appointments/me', {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then(response => {
+        setAppointments(response.data);
+      });
+  }, [selectedDate]);
+
   const disabledDays = useMemo(() => {
     const dates = monthAvailability
       .filter(monthDay => monthDay.available === false)
@@ -68,6 +97,14 @@ const Dashboard: React.FC = () => {
       });
     return dates;
   }, [currentMonth, monthAvailability]);
+
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Dia' dd 'de' MMMM ", { locale: ptBR });
+  }, [selectedDate]);
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, 'cccc', { locale: ptBR });
+  }, [selectedDate]);
 
   return (
     <Container>
@@ -92,12 +129,12 @@ const Dashboard: React.FC = () => {
         <Schedule>
           <h1>Horário agendados</h1>
           <p>
-            <span>Hoje</span>
-            <span>Dia 06</span>
-            <span>Segunda-feira</span>
+            {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDateAsText}</span>
+            <span>{selectedWeekDay}</span>
           </p>
           <NextAppointment>
-            <strong>Atendimento a seguir</strong>
+            <strong>Agendamento a seguir</strong>
             <div>
               <img
                 src="https://avatars.githubusercontent.com/u/30667729?s=460&u=e5e7f27ae6315a30400120e97b64a61ea4cc83fc&v=4"
@@ -114,7 +151,7 @@ const Dashboard: React.FC = () => {
           <Section>
             <strong>Manhã</strong>
 
-            <Appointment>
+            <IAppointment>
               <span>
                 <FiClock />
                 08:00
@@ -126,12 +163,12 @@ const Dashboard: React.FC = () => {
                 />
                 <strong>Olimpio Pimenta</strong>
               </div>
-            </Appointment>
+            </IAppointment>
           </Section>
 
           <Section>
             <strong>Tarde</strong>
-            <Appointment>
+            <IAppointment>
               <span>
                 <FiClock />
                 08:00
@@ -143,7 +180,7 @@ const Dashboard: React.FC = () => {
                 />
                 <strong>Olimpio Pimenta</strong>
               </div>
-            </Appointment>
+            </IAppointment>
           </Section>
         </Schedule>
         <Calendar>
