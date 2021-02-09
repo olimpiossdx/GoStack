c6ef011fd9b16c0600f28151b8a8b8778347da3a
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-09 20:16:33 -0300

    Header do dashboard

--- nivel-3/appgobarber/src/hooks/auth.tsx
+++ nivel-3/appgobarber/src/hooks/auth.tsx
@@ -3,9 +3,17 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredenials {
@@ -14,7 +22,7 @@ interface SignInCredenials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   singIn(credentials: SignInCredenials): Promise<void>;
   signOut(): void;
