447ec894e36146861122944b6030c9a805dc2ee5
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-11 21:26:52 -0300

    Correção de erro de build

---
+++ appgobarber/src/App.tsx
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar, View } from 'react-native';
+import AppProvider from './hooks';
+import Routes from './routes';
+
+const App = () => {
+  return (<NavigationContainer>
+    <StatusBar barStyle='light-content' backgroundColor='#312e38' />
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </AppProvider>
+  </NavigationContainer>);
+}
+
+
+export default App;
--- nivel-3/appgobarber/README.md
+++
@@ -1,3 +0,0 @@
-# GoBarber aplicativo
-
-<img alt='app GoBarber' title='BootCamp' src='../../.github/app_gobarber.jpg' />
