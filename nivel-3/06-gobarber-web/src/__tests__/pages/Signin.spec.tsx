dce1441cc24dcbf4e10943b3e91341610d53e5c3
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-14 11:09:31 -0300

    limpeza de código

--- nivel-3/06-gobarber-web/src/__tests__/pages/Signin.spec.tsx
+++ nivel-3/06-gobarber-web/src/__tests__/pages/Signin.spec.tsx
@@ -14,6 +14,7 @@ jest.mock('react-router-dom', () => {
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
+
 jest.mock('../../hooks/auth', () => {
   return {
     useAuth: () => ({
