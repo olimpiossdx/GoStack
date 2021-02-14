8b63490e9479af74828a9328b47df5871b492f75
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-14 10:31:45 -0300

    teste de login

--- nivel-3/06-gobarber-web/src/__tests__/pages/Signin.spec.tsx
+++ nivel-3/06-gobarber-web/src/__tests__/pages/Signin.spec.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { debug } from 'console';
+import { fireEvent, render } from '@testing-library/react';
 import SignIn from '../../pages/Signin';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
   it('should be albe to sign in', () => {
-    const result = render(<SignIn />);
-    debug();
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'jonhdoe@example.com.br' },
+    });
+
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
