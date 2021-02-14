5ff705852244906ee419aaf09f09836a4826f768
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-14 11:06:29 -0300

    finalizando testes de login

--- nivel-3/06-gobarber-web/src/__tests__/pages/Signin.spec.tsx
+++ nivel-3/06-gobarber-web/src/__tests__/pages/Signin.spec.tsx
@@ -3,6 +3,8 @@ import { fireEvent, render, waitFor } from '@testing-library/react';
 import SignIn from '../../pages/Signin';
 
 const mockedHistoryPush = jest.fn();
+const mockedSignIn = jest.fn();
+const mockedAddToast = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
@@ -12,16 +14,28 @@ jest.mock('react-router-dom', () => {
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
-
 jest.mock('../../hooks/auth', () => {
   return {
     useAuth: () => ({
-      signIn: jest.fn(),
+      signIn: mockedSignIn,
+    }),
+  };
+});
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
     }),
   };
 });
 
 describe('SignIn Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+    mockedSignIn
+  });
+
   it('should be albe to sign in', async () => {
     const { getByPlaceholderText, getByText } = render(<SignIn />);
 
@@ -39,6 +53,54 @@ describe('SignIn Page', () => {
 
     await waitFor(() => {
       expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+  });
+
+  it('should not be albe to sign in with invalid credencial', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'not-vaild-email' },
+    });
+
+    fireEvent.change(passwordField, {
+      target: { value: 'not-vaild-password' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toBeCalledWith();
+    });
+  });
+
+  it('should display an error if login fails', async () => {
+    mockedSignIn.mockImplementation(() => {
+      throw new Error();
+    });
+
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'jonhdoe@example.com.br' },
+    });
+
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'error' }),
+      );
     });
   });
 });
